3d5b5e12bc795e447ae274ab4629a4fb623046f0
Weaver Goldman <we.goldm@gmail.com>
2024-06-23 13:16:35 -0400

    Gain 4x speedup from functional parallelization.

--- ouroboros/pipeline/save_parallel_pipeline.py
+++ ouroboros/pipeline/save_parallel_pipeline.py
@@ -42,72 +42,76 @@ class SaveParallelPipelineStep(PipelineStep):
         data_queue = multiprocessing.Queue()
         download_done_event = multiprocessing.Event()
 
-        # Thread pool executor to download the data
-        with concurrent.futures.ThreadPoolExecutor(max_workers=self.num_threads) as download_executor:
-            download_futures = []
+        # Start the download volumes process
+        download_volumes(volume_cache, data_queue, download_done_event, self.num_threads)
 
-            for i in range(len(volume_cache.volumes)):
-                download_future = download_executor.submit(thread_worker, volume_cache, i, data_queue, download_done_event)
-                download_futures.append(download_future)
+        # Process pool executor to process the data
+        with concurrent.futures.ProcessPoolExecutor(max_workers=self.num_processes) as process_executor:
+            futures = []
+
+            print("Starting processing")
             
-            # Process pool executor to process the data
-            with concurrent.futures.ProcessPoolExecutor(max_workers=self.num_processes) as process_executor:
-                futures = []
-                
-                while True:
-                    try:
-                        print("Starting process")
-                        data = data_queue.get(timeout=1)
-                        futures.append(process_executor.submit(process_worker, config, data, slice_rects, self.num_threads))
-                    except multiprocessing.queues.Empty:
-                        if download_done_event.is_set() and data_queue.empty():
-                            print("done")
-                            break
-                
-                for future in concurrent.futures.as_completed(futures):
-                    try:
-                        vol_index = future.result()
-                    except Exception as e:
-                        print("Error processing data!!!")
-                        return None, f"An error occurred while processing data: {e}"
-                    else:
-                        # Delete the volume used in the child process
-                        volume_cache.remove_volume(vol_index)
-                        print(f"Remove volume {vol_index}")
+            while True:
+                try:
+                    data = data_queue.get(timeout=1)
+                    futures.append(process_executor.submit(process_worker, config, data, slice_rects, self.num_threads))
+                except multiprocessing.queues.Empty:
+                    if download_done_event.is_set() and data_queue.empty():
+                        print("done downloading")
+                        break
+            
+            for future in concurrent.futures.as_completed(futures):
+                try:
+                    vol_index = future.result()
+                except Exception as e:
+                    print("Error processing data!!!")
+                    return None, f"An error occurred while processing data: {e}"
+                else:
+                    # Delete the volume used in the child process
+                    volume_cache.remove_volume(vol_index)
+                    print(f"Remove volume {vol_index}")
 
-                print ("Done processing")
-
-            for download_future in concurrent.futures.as_completed(download_futures):
-                download_future.result()
-            print("Done 2")
-
-        print("Done 3")
+            print ("Done processing")
 
         # Load the saved tifs in numerical order
         tif_files = get_sorted_tif_files(folder_name)
-        print(tif_files)
 
         # Save tifs to a new resulting tif 
         with TiffWriter(config.output_file_path) as tif:
             for filename in tif_files:
-                tif_file = imread(filename)
+                tif_file = imread(f"{config.output_file_path}-slices/{filename}")
                 tif.write(tif_file, contiguous=True)
 
         # Delete slices folder
-        # shutil.rmtree(folder_name)
+        shutil.rmtree(folder_name)
 
         return config.output_file_path, None
 
+
 def thread_worker(volume_cache, i, data_queue, download_done_event):
-    try:
-        print(f"Downloading volume {i}")
-        data = volume_cache.create_processing_data(i)
-        data_queue.put(data)
-    except Exception as e:
-        print(f"Error downloading volume {i}: {e}")
-    finally:
-        if i == (len(volume_cache.volumes) - 1):
-            download_done_event.set()
+        try:
+            print(f"Downloading volume {i}")
+            data = volume_cache.create_processing_data(i)
+            data_queue.put(data)
+        except Exception as e:
+            print(f"Error downloading volume {i}: {e}")
+        finally:
+            if i == (len(volume_cache.volumes) - 1):
+                download_done_event.set()
+
+def download_volumes(volume_cache, data_queue, download_done_event, num_threads):
+    with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as download_executor:
+        download_futures = []
+
+        for i in range(len(volume_cache.volumes)):
+            download_future = download_executor.submit(thread_worker, volume_cache, i, data_queue, download_done_event)
+            download_futures.append(download_future)
+        
+        for download_future in concurrent.futures.as_completed(download_futures):
+            download_future.result()
+        
+        # Ensure the event is set even if all volumes are downloaded
+        download_done_event.set()
 
 def process_worker(config, processing_data, slice_rects, num_threads):
     volume, bounding_box, slice_indices, volume_index = processing_data
@@ -127,7 +131,6 @@ def process_worker(config, processing_data, slice_rects, num_threads):
     return volume_index
 
 def save_thread(filename, data):
-    # print("saving", filename)
     imwrite(filename, data)
 
 def get_sorted_tif_files(directory):
