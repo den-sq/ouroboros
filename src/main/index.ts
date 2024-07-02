49b361cc37895c39989eb8e14b2642860ad7ed04
Weaver Goldman <we.goldm@gmail.com>
2024-07-02 12:00:36 -0400

    Improve window behavior with electron app.

--- src/main/index.ts
+++ src/main/index.ts
@@ -4,10 +4,7 @@ import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
 
 function createWindow(): void {
-	// Create the browser window.
 	const mainWindow = new BrowserWindow({
-		width: 900,
-		height: 670,
 		show: false,
 		autoHideMenuBar: true,
 		...(process.platform === 'linux' ? { icon } : {}),
@@ -18,7 +15,8 @@ function createWindow(): void {
 	})
 
 	mainWindow.on('ready-to-show', () => {
-		mainWindow.show()
+		mainWindow.maximize()
+		mainWindow.focus()
 	})
 
 	mainWindow.webContents.setWindowOpenHandler((details) => {
@@ -35,9 +33,6 @@ function createWindow(): void {
 	}
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
 	// Set app user model id for windows
 	electronApp.setAppUserModelId('com.electron')
@@ -61,14 +56,6 @@ app.whenReady().then(() => {
 	})
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
-		app.quit()
-	}
+	app.quit()
 })
-
-// In this file you can include the rest of your app"s specific main process
-// code. You can also put them in separate files and require them here.
