6bccabc6cd7bae88d7967e061ac347fd2c1a591e
Weaver Goldman <we.goldm@gmail.com>
2024-07-26 15:13:32 -0400

    Reorganize tests.

--- python/test/helpers/test_spline.py
+++ python/test/helpers/test_spline.py
@@ -92,6 +92,54 @@ def test_vectors_orthogonality():
         ), "Normal and binormal vectors should be orthogonal"
 
 
+def test_rotation_minimizing_vectors():
+    # Define a simple curve as sample points
+    sample_points = np.array([[1, 0, 0], [1, 2, 1], [2, 1, 2], [3, -2, 4], [4, 7, 4]])
+
+    # Initialize Spline object
+    spline = Spline(sample_points, degree=3)
+
+    times = np.linspace(0, 1, 100)
+
+    # Calculate rotation minimizing vectors
+    tangent_vectors, normal_vectors, binormal_vectors = (
+        spline.calculate_rotation_minimizing_vectors(times)
+    )
+
+    # Transpose the vectors for vector-by-vector indexing (3, n) -> (n, 3)
+    tangent_vectors = tangent_vectors.T
+    normal_vectors = normal_vectors.T
+    binormal_vectors = binormal_vectors.T
+
+    # Check orthogonality between each pair of vectors
+    for i in range(tangent_vectors.shape[0]):
+        tangent_normal_dot = np.dot(tangent_vectors[i], normal_vectors[i])
+        tangent_binormal_dot = np.dot(tangent_vectors[i], binormal_vectors[i])
+        normal_binormal_dot = np.dot(normal_vectors[i], binormal_vectors[i])
+
+        assert np.allclose(
+            tangent_normal_dot, 0, atol=1e-6
+        ), "Tangent and normal vectors should be orthogonal"
+        assert np.allclose(
+            tangent_binormal_dot, 0, atol=1e-6
+        ), "Tangent and binormal vectors should be orthogonal"
+        assert np.allclose(
+            normal_binormal_dot, 0, atol=1e-6
+        ), "Normal and binormal vectors should be orthogonal"
+
+    # Ensure that vectors do not flip to the other side of the spline
+    for i in range(1, tangent_vectors.shape[0]):
+        assert (
+            np.dot(tangent_vectors[i - 1], tangent_vectors[i]) > 0
+        ), "Tangent vectors should not flip"
+        assert (
+            np.dot(normal_vectors[i - 1], normal_vectors[i]) > 0
+        ), "Normal vectors should not flip"
+        assert (
+            np.dot(binormal_vectors[i - 1], binormal_vectors[i]) > 0
+        ), "Binormal vectors should not flip"
+
+
 # def test_calculate_equidistant_parameters():
 #     # Define a simple curve as sample points
 #     sample_points = np.array([
