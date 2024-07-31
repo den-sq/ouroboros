d0e65f0812be8113d86f9e482ac35aeee964fdbf
Weaver Goldman <we.goldm@gmail.com>
2024-07-31 11:12:12 -0400

    Allow fullscreen plugins.

--- src/renderer/src/components/PluginDisplay/PluginDisplay.tsx
+++ src/renderer/src/components/PluginDisplay/PluginDisplay.tsx
@@ -4,6 +4,7 @@ function PluginDisplay({ url }: { url: string }): JSX.Element {
 			src={url}
 			style={{ width: '100%', height: '100%', border: 'none' }}
 			scrolling="no"
+			allowFullScreen
 		/>
 	)
 }
