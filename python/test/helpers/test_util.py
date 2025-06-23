a1ffc62fe29bfe7c9dcc8df73c53971f29c61362
David Northover <60146072+den-sq@users.noreply.github.com>
2025-06-23 18:15:55 -0400

    Fix Util tests and bugs
    - Proper bit counting for unique_lv to not 'lose' max values.
    - Proper bit shift for unique_lv to not scramble values when int types don't match.

--- python/test/helpers/test_util.py
+++ python/test/helpers/test_util.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from ouroboros.helpers.util import unique_high_dup, unique_lv
+from ouroboros.helpers.util import unique_lv
 
 
 def test_unique():
@@ -8,22 +8,15 @@ def test_unique():
 
     result, counts = np.unique(data, False, False, True, axis=1)
 
-    i_result, i_counts = unique_high_dup(data, return_counts=True)
-
     lv_result, lv_counts = unique_lv(data, return_counts=True)
 
     print(data.shape)
     print(result.shape)
-    print(i_result.shape)
     print(lv_result.shape)
 
     print(data[:, :20])
     print(result[:, :20])
-    print(i_result[:, :20])
     print(lv_result[:, :20])
-
-    assert np.all(i_result == result)
-    assert np.all(i_counts == counts)
 
     assert np.all(lv_result == result)
     assert np.all(lv_counts == counts)
